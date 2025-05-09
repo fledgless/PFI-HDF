f49d21089f5aa9eaf7e1cb4b2f4108184dfa2f1b
fledgless <cameriancardinal@gmail.com>
2025-05-09 19:55:09 +0200

    filled dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,44 +7,48 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Entity\Page;
+use App\Entity\Article;
+use App\Entity\Category;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // 1.1) If you have enabled the "pretty URLs" feature:
-        // return $this->redirectToRoute('admin_user_index');
-        //
-        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Pfi Cfp Ste Barbe');
+            ->setTitle('Pole Insertion Formation');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Gestion des utilisateurs');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class)
+            ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Nouvel utilisateur', 'fa fa-user-plus', User::class)
+            ->setAction('new')
+            ->setPermission('ROLE_ADMIN');
+
+        yield MenuItem::section('Gestion des pages');
+        yield MenuItem::linkToCrud('Pages', 'fa fa-file', Page::class);
+        yield MenuItem::linkToCrud('Nouvelle page', 'fa fa-plus', Page::class)
+            ->setAction('new');
+
+        yield MenuItem::section('Gestion des catégories');
+        yield MenuItem::linkToCrud('Catégories', 'fa fa-folder', Category::class);
+        yield MenuItem::linkToCrud('Nouvelle catégorie', 'fa fa-folder-plus', Category::class)
+            ->setAction('new');
+
+        yield MenuItem::section('Gestion des articles');
+        yield MenuItem::linkToCrud('Articles', 'fa fa-newspaper', Article::class);
+        yield MenuItem::linkToCrud('Nouvel article', 'fa fa-plus', Article::class)
+            ->setAction('new');
     }
 }
