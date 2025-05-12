13a210111a3fac28124f834b52775dad17cf6956
fledgless <cameriancardinal@gmail.com>
2025-05-12 11:21:49 +0200

    added use case diagram

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -4,10 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints\Date;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -19,12 +22,16 @@ class ArticleCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('titre');
-        yield TextField::new('slug');
+        yield SlugField::new('slug')
+            ->setTargetFieldName('titre')
+            ->hideOnIndex();
         yield TextEditorField::new('contenu')->hideOnIndex();
         yield ImageField::new('nomFichierMiniature')
             ->setBasePath('uploads/articles')
             ->setUploadDir('public/uploads/articles')
             ->setLabel('Image miniature');
         yield TextField::new('texteAlternatifMiniature')->hideOnIndex();
+        yield DateTimeField::new('createdAt')->hideOnForm();
+        yield DateTimeField::new('updatedAt')->hideOnForm();
     }
 }
--- src/Controller/Admin/PageCrudController.php
+++ src/Controller/Admin/PageCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -19,15 +20,18 @@ class PageCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield IdField::new('id')->hideOnForm();
         yield TextField::new('titre');
-        yield TextField::new('slug');
-        yield TextEditorField::new('contenu')->hideOnIndex();
+        yield SlugField::new('slug')
+            ->setTargetFieldName('titre')
+            ->hideOnIndex();
+        yield TextEditorField::new('contenu')
+            ->hideOnIndex();
         yield ImageField::new('nomFichierMiniature')
             ->setBasePath('uploads/pages')
             ->setUploadDir('public/uploads/pages')
             ->setLabel('Image miniature');
-        yield TextField::new('texteAlternatifMiniature')->hideOnIndex();
+        yield TextField::new('texteAlternatifMiniature')
+            ->hideOnIndex();
         yield AssociationField::new('category')
             ->setRequired(true)
             ->setLabel('Catégorie')
